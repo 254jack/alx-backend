ef1ff1811af042512807dee37f7b02e7ee943f4d
254jack <taixonj@gmail.com>
2024-04-03 23:45:35 +0300

    first commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''Task 0: Basic dictionary
+'''
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''A class `BasicCache`
+    '''
+
+    def put(self, key, item):
+        '''assign to the self dictionary
+        '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''return the value
+        '''
+
+        return self.cache_data.get(key, None)
+    
